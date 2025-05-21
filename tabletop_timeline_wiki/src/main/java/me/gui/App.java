9e6a96758a80e1231671f6164cecb8b4d0594da1
NathanC <nathaniel.christian2077@gmail.com>
2025-05-21 09:30:57 -0300

    css changes on login scene

--- tabletop_timeline_wiki/src/main/java/me/gui/App.java
+++ tabletop_timeline_wiki/src/main/java/me/gui/App.java
@@ -16,7 +16,6 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        Font.loadFont(getClass().getResource("fonts/Cinzel-Regular.ttf").toExternalForm(), 10);
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Login.fxml"));
         Parent root = fxmlLoader.load();
         Scene scene = new Scene(root, 1280, 720);
@@ -24,6 +23,8 @@ public class App extends Application {
         stage.setTitle("Codex Core - Login");
         stage.setScene(scene);
         stage.setResizable(true);
+        stage.setMinWidth(640);
+        stage.setMinHeight(480);
         stage.show();
     }
 
